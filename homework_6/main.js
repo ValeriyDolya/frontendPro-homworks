97ab9bf46f53059c81aecbffc45a7c9341825d3d
ValeriyDolya <101823334+ValeriyDolya@users.noreply.github.com>
2022-04-12 20:00:42 +0300

    Update main.js

--- homework_6/main.js
+++ homework_6/main.js
@@ -1,22 +1,19 @@
 function inputNumber() {
     let number = Number(prompt('write here.'));
-    function evenOrOdd(inputNumber) {
+    function evenOrOdd() {
         if (number % 2 === 0) {
             return ("even");
         }else {
             return ("odd");
         }
     }
-    function checkIfNumberPrime(inputNumber) {
+    function checkIfNumberPrime() {
         for (let i = 2; i < number; i++) {
             if (number % i === 0) {
-                return ("composite");               
-            } else {
-                return ("prime");    
-            } 
-        }  
-    }
-    evenOrOdd();
-    checkIfNumberPrime();
+                return ("composit");
+            }
+        }
+        return ("prime");
+    }    
     alert(`Entered number is ${evenOrOdd()} and ${checkIfNumberPrime()}`);
 }
